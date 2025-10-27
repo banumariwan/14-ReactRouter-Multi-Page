542193239e46d717d88df2fdafc9800c6eeb9a93
banumariwan <mariwanburhan5@gmail.com>
2025-10-27 03:40:33 +0300

    Project 14: React Router - Multi-page Static Site

---
+++ project-14-react-router/src/pages/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <h1>ℹ️ About Us</h1>
+      <p>We build modern web apps using React. Our goal is to make coding fun and accessible!</p>
+    </div>
+  );
+}
+
+export default About;
---
+++ project-14-react-router/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h1>🏡 Welcome to MyCompany</h1>
+      <p>This is the homepage. Navigate using the navbar above!</p>
+    </div>
+  );
+}
+
+export default Home;
